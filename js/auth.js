982890a74517b1ee1d4a93b171bbadc8fd0e1d26
ahmadrashad647-dot <ahmadrashad647@gmail.com>
2026-02-03 01:43:47 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,4 @@
+console.log("AUTH FILE LOADED");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { firebaseConfig } from "./firebase-config.js";
