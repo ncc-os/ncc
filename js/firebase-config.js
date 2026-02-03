0dbcc62ea688942d8932622ddf11f1474adb4fb3
ahmed <ahmadrashad647@gmail.com>
2026-02-04 00:04:58 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,18 @@
-// Firebase SDKs from CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
+// firebase-config.js - Initialize Firebase v8 (CDN version)
 const firebaseConfig = {
   apiKey: "AIzaSyCrlP360Fpx-9tCyggJeoqLgkwnTlRTJ5o",
   authDomain: "ncc-project-3b0f1.firebaseapp.com",
   projectId: "ncc-project-3b0f1",
-  storageBucket: "ncc-project-3b0f1.appspot.com",
+  storageBucket: "ncc-project-3b0f1.firebasestorage.app",
   messagingSenderId: "590441541895",
   appId: "1:590441541895:web:2a861830fcb60f09f655a2"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Prevent duplicate initialization
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Export for use in other files
+const db = firebase.firestore();
+const auth = firebase.auth();
